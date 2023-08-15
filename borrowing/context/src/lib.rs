b03032a49c99498b55fa79c17a2caa7a43982949
Kirill Kiselev <lirikk285@mail.ru>
2023-08-15 15:18:50 +0300

    add context task

--- borrowing/context/src/lib.rs
+++ borrowing/context/src/lib.rs
@@ -1,17 +1,41 @@
 #![forbid(unsafe_code)]
 
+use std::{
+    any::{Any, TypeId},
+    collections::HashMap,
+};
+
+#[derive(Default)]
 pub struct Context {
-    // TODO: your code goes here.
+    map: HashMap<String, Box<dyn Any>>,
+    singletone_map: HashMap<TypeId, Box<dyn Any>>,
 }
 
 impl Context {
     pub fn new() -> Self {
-        // TODO: your code goes here.
-        unimplemented!()
+        Self {
+            map: HashMap::new(),
+            singletone_map: HashMap::new(),
+        }
     }
 
-    // fn insert(&mut self, key: ..., obj: ...);
-    // fn get(&self, key: ...);
-    // fn insert_singletone(&mut self, obj: ...);
-    // fn get_singletone(&self);
+    pub fn insert<K: ToString, Q: Any>(&mut self, key: K, obj: Q) {
+        self.map.insert(key.to_string(), Box::new(obj));
+    }
+
+    pub fn get<Q: Any>(&self, key: &str) -> &Q {
+        self.map.get(key).unwrap().downcast_ref().unwrap()
+    }
+
+    pub fn insert_singletone<Q: Any>(&mut self, obj: Q) {
+        self.singletone_map.insert(obj.type_id(), Box::new(obj));
+    }
+
+    pub fn get_singletone<Q: Any>(&self) -> &Q {
+        self.singletone_map
+            .get(&TypeId::of::<Q>())
+            .unwrap()
+            .downcast_ref()
+            .unwrap()
+    }
 }
